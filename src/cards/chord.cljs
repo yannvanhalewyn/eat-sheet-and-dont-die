2feaf0dac74b813620b74df913b9d0ff280a4e23
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-12 22:36:55 +0100

    add defcard-with-props macro

--- src/cards/chord.cljs
+++ src/cards/chord.cljs
@@ -1,33 +1,34 @@
 (ns cards.chord
   (:require [reagent.core :as reagent]
             [sheet-bucket.components.chord :as chord])
-  (:require-macros [devcards.core :as dc :refer [defcard defcard-doc]]))
+  (:require-macros [devcards.core :refer [defcard-doc]]
+                   [cards.core :refer [defcard-with-props]]))
 
 (defn- chord [props]
   (reagent/as-element (chord/component props)))
 
 (defcard-doc
-  "# Chord 
+  "# Chord
 ## Example props"
   {:root "G" :triad :major :seventh :minor :nineth :major}
   "## An editable chord"
   {:focused true :chord-text "abc"})
 
 ;; Triads
-(defcard Major (chord {:root "a"}))
-(defcard Minor (chord {:root "b" :triad :minor}))
+(defcard-with-props Major (chord {:root "a"}))
+(defcard-with-props Minor (chord {:root "b" :triad :minor}))
 
 ;; Sevenths
-(defcard Seventh (chord {:root "C" :triad :major :seventh :minor}))
-(defcard Minor-Seventh (chord {:root "D" :triad :minor :seventh :minor}))
-(defcard Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
-(defcard Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
-(defcard Minor-Major-Seventh (chord {:root "E" :triad :minor :seventh :major}))
+(defcard-with-props Seventh (chord {:root "C" :triad :major :seventh :minor}))
+(defcard-with-props Minor-Seventh (chord {:root "D" :triad :minor :seventh :minor}))
+(defcard-with-props Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
+(defcard-with-props Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
+(defcard-with-props Minor-Major-Seventh (chord {:root "E" :triad :minor :seventh :major}))
 
 ;; Nineths
-(defcard Nineth (chord {:root "F" :triad :major :seventh :minor :nineth :minor}))
-(defcard Minor-Nineth (chord {:root "F" :triad :minor :seventh :minor :nineth :minor}))
-(defcard Major-Nineth (chord {:root "G" :triad :major :seventh :minor :nineth :major}))
+(defcard-with-props Nineth (chord {:root "F" :triad :major :seventh :minor :nineth :minor}))
+(defcard-with-props Minor-Nineth (chord {:root "F" :triad :minor :seventh :minor :nineth :minor}))
+(defcard-with-props Major-Nineth (chord {:root "G" :triad :major :seventh :minor :nineth :major}))
 
 ;; Editing
-(defcard Focused (chord {:focused true :chord-text "a-maj"}))
+(defcard-with-props Focused (chord {:focused true :chord-text "a-maj"}))
---
+++ src/cards/core.clj
@@ -0,0 +1,16 @@
+(ns cards.core
+  (:import java.io.StringWriter))
+
+(defmacro defcard-with-props
+  "A defcard macro to help keep the card's props and prop
+  documentation in sync. Will transform into a defcard-rg call with an
+  optional message concatinated to a markdown clojure code block of
+  the props followed by the component declaration itself."
+  ([name [component props]] (list `defcard-with-props name "" [component props]))
+  ([name msg [component props]]
+   (list 'devcards.core/defcard-rg name
+         (str msg
+              "\n```clojure\n"
+              (with-out-str (clojure.pprint/pprint props))
+              "\n```")
+         [component props])))
