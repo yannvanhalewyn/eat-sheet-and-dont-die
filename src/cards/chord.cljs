3fd9e9a26e74bef6966c97e3fe5800321ff150c9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-12 22:39:44 +0100

    Correctly use defcard-with-props

--- src/cards/chord.cljs
+++ src/cards/chord.cljs
@@ -1,11 +1,8 @@
 (ns cards.chord
   (:require [reagent.core :as reagent]
-            [sheet-bucket.components.chord :as chord])
+            [sheet-bucket.components.chord :refer [component]])
   (:require-macros [devcards.core :refer [defcard-doc]]
                    [cards.core :refer [defcard-with-props]]))
-
-(defn- chord [props]
-  (reagent/as-element (chord/component props)))
 
 (defcard-doc
   "# Chord
@@ -15,20 +12,36 @@
   {:focused true :chord-text "abc"})
 
 ;; Triads
-(defcard-with-props Major (chord {:root "a"}))
-(defcard-with-props Minor (chord {:root "b" :triad :minor}))
+(defcard-with-props Major [component {:root "a"}])
+(defcard-with-props Minor [component {:root "b" :triad :minor}])
 
 ;; Sevenths
-(defcard-with-props Seventh (chord {:root "C" :triad :major :seventh :minor}))
-(defcard-with-props Minor-Seventh (chord {:root "D" :triad :minor :seventh :minor}))
-(defcard-with-props Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
-(defcard-with-props Major-Seventh (chord {:root "E" :triad :major :seventh :major}))
-(defcard-with-props Minor-Major-Seventh (chord {:root "E" :triad :minor :seventh :major}))
+(defcard-with-props Seventh
+  [component {:root "C" :triad :major :seventh :minor}])
+
+(defcard-with-props Minor-Seventh
+  [component {:root "D" :triad :minor :seventh :minor}])
+
+(defcard-with-props Major-Seventh
+  [component {:root "E" :triad :major :seventh :major}])
+
+(defcard-with-props Major-Seventh
+  [component {:root "E" :triad :major :seventh :major}])
+
+(defcard-with-props Minor-Major-Seventh
+  [component {:root "E" :triad :minor :seventh :major}])
 
 ;; Nineths
-(defcard-with-props Nineth (chord {:root "F" :triad :major :seventh :minor :nineth :minor}))
-(defcard-with-props Minor-Nineth (chord {:root "F" :triad :minor :seventh :minor :nineth :minor}))
-(defcard-with-props Major-Nineth (chord {:root "G" :triad :major :seventh :minor :nineth :major}))
+(defcard-with-props Nineth
+  [component {:root "F" :triad :major :seventh :minor :nineth :minor}])
+
+(defcard-with-props Minor-Nineth
+  [component {:root "F" :triad :minor :seventh :minor :nineth :minor}])
+
+(defcard-with-props Major-Nineth
+  [component {:root "G" :triad :major :seventh :minor :nineth :major}])
 
 ;; Editing
-(defcard-with-props Focused (chord {:focused true :chord-text "a-maj"}))
+(defcard-with-props Focused
+  "This chord should be focused on page load."
+  [component {:focused true :chord-text "a-maj"}])
