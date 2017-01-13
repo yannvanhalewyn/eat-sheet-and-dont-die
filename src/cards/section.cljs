ca5d8e61d05954d806409e949cca0c3f7e7842d7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-13 22:05:05 +0100

    Flow handlers from section to chords

--- src/cards/section.cljs
+++ src/cards/section.cljs
@@ -11,7 +11,20 @@
   "Section has rows of bars bla bla"
   "## Sample props"
   {:name "Section Name"
-   :rows rows})
+   :rows (cons (take 1 rows) [["..."]])
+   :on-chord-click 'click-fn
+   :on-chord-update 'update-fn
+   :selected (-> rows first second first)})
 
 (defcard-props base
-  [subject/component {:name "Intro" :rows rows}])
+  "Should launch an alert with the chord ID on click"
+  [subject/component
+   {:name "Intro" :rows rows :on-chord-click js/alert}])
+
+(def on-chord-update #(js/alert (str %1 " with " %2)))
+(defcard-props with-current-chord
+  "Should display the first chord of the second bar as editable (if there)"
+  [subject/component {:name "Intro"
+                      :rows rows
+                      :selected (-> rows first second first :id)
+                      :on-chord-update on-chord-update}])
--- src/sheet_bucket/components/section.cljs
+++ src/sheet_bucket/components/section.cljs
@@ -2,13 +2,13 @@
   (:require [sheet-bucket.components.bar :as bar])
   (:require-macros [sheet-bucket.util.util :refer [fori]]))
 
-(defn row-component [{:keys [bars]}]
+(defn row-component [{:keys [bars] :as props}]
   [:div.row {:style {:margin-bottom "10px" :white-space :nowrap}}
    (fori [i bar bars]
-     ^{:key i} [bar/component {:chords bar}])])
+     ^{:key i} [bar/component (-> props (dissoc :bars) (assoc :chords bar))])])
 
-(defn component [{:keys [name rows]}]
+(defn component [{:keys [name rows] :as props}]
   [:div.section
    [:h3 name]
    (fori [i row rows]
-     ^{:key i} [row-component {:bars row}])])
+     ^{:key i} [row-component (-> props (dissoc :name :rows) (assoc :bars row))])])
