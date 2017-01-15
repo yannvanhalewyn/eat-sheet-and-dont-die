e8fcfed6c49b40b535767f186982fb5fac8c7eaa
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-15 01:38:43 +0100

    Make chord cards render all roots dynamically

--- src/cards/chord.cljs
+++ src/cards/chord.cljs
@@ -1,9 +1,11 @@
 (ns cards.chord
   (:require [cards.util :refer [alert]]
-            [sheet-bucket.models.chord :refer [parse]]
+            [sheet-bucket.models.chord :refer [parse gen]]
             [sheet-bucket.components.chord :refer [displayed-chord editable-chord]]
+            [devcards.core :refer [markdown->react]]
             [reagent.core :as r])
   (:require-macros [devcards.core :refer [defcard-doc defcard-rg]]
+                   [sheet-bucket.util.util :refer [fori]]
                    [cards.core :refer [defcard-props]]))
 
 (defcard-doc
@@ -26,62 +28,59 @@
 (defcard-doc
   "## Display Chord"
   "This is a chord in it's rest state. This is what the user will see
-  when it's not editing the chord.")
+  when it's not editing the chord."
+  "### props"
+  (first (gen 1)))
 
 (def roots ["A" "B" "C" "D" "E" "F" "G" "1" "2" "3" "4" "5" "6" "7"])
-
-(defcard-rg roots
-  "All the natural roots"
-  [:div {:style {:display :flex :justify-content :space-between}}
-   (for [root roots]
-     ^{:key root} [displayed-chord {:root [root]}])])
-
-(defcard-rg flats
-  "All the flat roots"
-  [:div {:style {:display :flex :justify-content :space-between}}
-   (for [root roots]
-     ^{:key root} [displayed-chord {:root [root :flat]}])])
-
-(defcard-rg sharps
-  "All the sharp roots"
-  [:div {:style {:display :flex :justify-content :space-between}}
-   (for [root roots]
-     ^{:key root} [displayed-chord {:root [root :sharp]}])])
 
 (defcard-props Major
   "Clicking the chord should launch an alert"
   [displayed-chord {:root ["A" :flat] :on-click (alert "click")}])
 
+(defn render-multi [chords]
+  [:div {:style {:display :flex :justify-content :space-between}}
+   (fori [i chord chords]
+     ^{:key i} [displayed-chord chord])])
+
+(defn for-all-roots [attrs title]
+  [:div
+   (if title [:h2 {:style {:color "#333"}} title])
+   [markdown->react (merge {:root ['root]} attrs)]
+   [render-multi (for [root roots] (merge {:root [root]} attrs))]])
+
+(defcard-rg roots
+  "All the natural roots"
+  [render-multi (map (fn [root] {:root [root]}) roots)])
+
+(defcard-rg flats
+  "All the flat roots"
+  [render-multi (map (fn [root] {:root [root :flat]}) roots)])
+
+(defcard-rg sharps
+  "All the sharp roots"
+  [render-multi (map (fn [root] {:root [root :sharp]}) roots)])
+
 ;; Triads
-(defcard-props Minor
-  "Note: root is case-insensitive"
-  [displayed-chord {:root ["B"] :triad :minor}])
+(defcard-rg Triads
+  [:div
+   [for-all-roots {:triad :minor} "Minor triads"]
+   [for-all-roots {:triad :augmented} "Augmented triads"]])
 
 ;; Sevenths
-(defcard-props Seventh
-  [displayed-chord {:root ["C"] :triad :major :seventh :minor}])
-
-(defcard-props Minor-Seventh
-  [displayed-chord {:root ["D"] :triad :minor :seventh :minor}])
-
-(defcard-props Major-Seventh
-  [displayed-chord {:root ["E"] :triad :major :seventh :major}])
-
-(defcard-props Major-Seventh
-  [displayed-chord {:root ["F"] :triad :major :seventh :major}])
-
-(defcard-props Minor-Major-Seventh
-  [displayed-chord {:root ["1"] :triad :minor :seventh :major}])
+(defcard-rg Sevenths
+  [:div
+   [for-all-roots {:seventh :minor} "Dominant seventh"]
+   [for-all-roots {:seventh :major} "Major seventh"]
+   [for-all-roots {:triad :minor :seventh :minor} "Minor sevenths"]
+   [for-all-roots {:triad :minor :seventh :major} "Minor major sevenths"]])
 
 ;; Ninths
-(defcard-props Ninth
-  [displayed-chord {:root ["F" :sharp] :triad :major :seventh :minor :ninth :minor}])
-
-(defcard-props Minor-Ninth
-  [displayed-chord {:root ["5"] :triad :minor :seventh :minor :ninth :minor}])
-
-(defcard-props Major-Ninth
-  [displayed-chord {:root ["3" :flat] :triad :major :seventh :minor :ninth :major}])
+(defcard-rg Ninths
+  [:div
+   [for-all-roots {:seventh :minor :ninth :natural} "Dominant ninths"]
+   [for-all-roots {:seventh :major :ninth :natural} "Major ninths"]
+   [for-all-roots {:triad :minor :seventh :minor :ninth :natural} "Minor ninths"]])
 
 (defcard-props No-root
   "Should not display anything"
