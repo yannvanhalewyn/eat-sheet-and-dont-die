0d413951d528236d15b5567f94304d188b636cfc
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-17 01:18:39 +0100

    Implement sheet#addBar

--- src/sheet_bucket/models/sheet.cljs
+++ src/sheet_bucket/models/sheet.cljs
@@ -1,13 +1,12 @@
 (ns sheet-bucket.models.sheet
-  (:require [clojure.zip :as zip :refer [end? next node]]))
+  (:require [clojure.zip :as zip :refer [up down right end? next node insert-right]]))
 
-(def new-sheet
-  [[;; Sections
-    [[;; Rows
-      [[;; Bars
-        [[{:id "1" :raw "edit-me"}]]]]]
-     {:name "Intro"}]]
-   {:title "Song name" :artist "Artist"}])
+(defn new-chord [id] {:id id :raw ""})
+(defn new-bar [chord-id] [[(new-chord chord-id)]])
+(defn new-row [chord-id] [[(new-bar chord-id)]])
+(defn new-section [chord-id] [[(new-row chord-id)] {:name "Intro"}])
+
+(def new-sheet [[(new-section "1")] {:artist "Artist" :title "Song name"}])
 
 (defn zipper
   "Builds the sheet zipper"
@@ -22,3 +21,10 @@
       (end? loc) nil
       (= id (:id (node loc))) loc
       :else (recur (next loc)))))
+
+(defn add-bar
+  "Takes a sheet zipped to the current chord, and adds a bar after the
+  current one. Returns the zipper zipped to the first chord of the new
+  bar."
+  [chord-loc new-chord-id]
+  (-> chord-loc up (insert-right (new-bar new-chord-id)) right down))
--- test/sheet_bucket/models/sheet_test.cljs
+++ test/sheet_bucket/models/sheet_test.cljs
@@ -1,8 +1,13 @@
 (ns sheet-bucket.models.sheet-test
-  (:require [sheet-bucket.models.sheet :refer [new-sheet zipper navigate-to]]
+  (:require [sheet-bucket.models.sheet :refer [new-sheet zipper navigate-to add-bar]]
             [cljs.test :refer-macros [deftest is]]
-            [clojure.zip :refer [node]]))
+            [clojure.zip :refer [node up children down]]))
 
-(deftest navigate-to
+(deftest navigateto
   (is (= "1" (-> new-sheet zipper (navigate-to "1") node :id)))
   (is (= nil (-> new-sheet zipper (navigate-to "2")))))
+
+(deftest addbar
+  (let [new-chord (-> new-sheet zipper (navigate-to "1") (add-bar "2"))]
+    (is (= 2 (-> new-chord up up children count)))
+    (is (= "2" (-> new-chord node :id)))))
