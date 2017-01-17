d76d598630c7e4820711365f4f6a21d6478064a7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-01-17 03:48:10 +0100

    Cleanup sheet test

--- test/sheet_bucket/models/sheet_test.cljs
+++ test/sheet_bucket/models/sheet_test.cljs
@@ -3,26 +3,28 @@
             [cljs.test :refer-macros [deftest is]]
             [clojure.zip :refer [node up left down children]]))
 
+(def test-loc (-> new-sheet zipper (navigate-to "1")))
+
 (deftest navigateTo
   (is (= "1" (-> new-sheet zipper (navigate-to "1") node :id)))
   (is (= nil (-> new-sheet zipper (navigate-to "2")))))
 
 (deftest addChord
-  (let [new-chord (-> new-sheet zipper (navigate-to "1") (append :chord "2"))]
+  (let [new-chord (-> test-loc (append :chord "2"))]
     (is (= 2 (-> new-chord up children count)))
     (is (= "2" (-> new-chord node :id)))))
 
 (deftest addBar
-  (let [new-chord (-> new-sheet zipper (navigate-to "1") (append :bar "2"))]
+  (let [new-chord (-> test-loc (append :bar "2"))]
     (is (= 2 (-> new-chord up up children count)))
     (is (= "2" (-> new-chord node :id)))))
 
 (deftest addRow
-  (let [new-chord (-> new-sheet zipper (navigate-to "1") (append :row "2"))]
+  (let [new-chord (-> test-loc (append :row "2"))]
     (is (= 2 (-> new-chord up up up children count)))
     (is (= "2" (-> new-chord node :id)))))
 
 (deftest addSection
-  (let [new-chord (-> new-sheet zipper (navigate-to "1") (append :section "2"))]
+  (let [new-chord (-> test-loc (append :section "2"))]
     (is (= 2 (-> new-chord up up up up children count)))
     (is (= "2" (-> new-chord node :id)))))
