d29985e9f91f1d7ce571289f3ce0cc497b8cce3c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-05 20:55:17 +0100

    Fix some navbar flex and add search bar in nav

--- src/sheet_bucket/components/layout/application.cljs
+++ src/sheet_bucket/components/layout/application.cljs
@@ -5,12 +5,17 @@
   [:div.u-max-height
    [:div.navbar
     [:div.navbar__home
-     [:a.navbar__item.u-block {:href "/#"}
+     [:a.navbar__item--icon.u-block {:href "/#"}
       [:i.material-icons "home"]]]
 
     [:div.navbar__breadcrumbs]
+    [:div.navbar__search
+     [:div.typeahead
+      [:div.typeahead__icon
+       [:i.material-icons "search"]]
+      [:input.typeahead__input {:type "text" :placeholder "Zoek..."}]]]
     [:div.navbar__right
-     [:div.navbar__item
+     [:div.navbar__item.navbar__item--icon
       [:i.material-icons "person"]]]]
    [:div.l-app.l-content
     [sheet/component props]]])
