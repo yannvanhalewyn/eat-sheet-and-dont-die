8b4d389064dd183ec62c9270ab9918b64c80020d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-08 22:36:48 +0100

    Hide re-frame registrar replacement warnings in development

--- env/dev/cljs/user.cljs
+++ env/dev/cljs/user.cljs
@@ -1,8 +1,19 @@
 (ns dev.user
   (:require [frontend.core :as app]
-            [devtools.core :as devtools]))
+            [devtools.core :as devtools]
+            [re-frame.loggers :as rf-loggers]))
 
 (devtools/install! [:custom-formatters :sanity-hints])
+
+(def warn #(.warn js/console %))
+
+;; Hide re-frame warnings like:
+;;   "re-frame: overwriting :event handler for: :subscription"
+;; when figwheel is reloading code
+(rf-loggers/set-loggers!
+  {:warn (fn [& args]
+           (when-not (= "re-frame: overwriting" (first args))
+             (apply warn args)))})
 
 (def on-js-load app/render!)
 
