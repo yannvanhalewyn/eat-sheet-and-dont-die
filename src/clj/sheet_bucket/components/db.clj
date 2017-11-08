f8e6fec8bc79ab90235ee12f5f6e4c35ea8a6394
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-08 01:37:53 +0100

    Make DB component

---
+++ src/clj/sheet_bucket/components/db.clj
@@ -0,0 +1,33 @@
+(ns sheet-bucket.components.db
+  (:require [com.stuartsierra.component :as c]
+            [datomic.client :as client]
+            [clojure.core.async :refer [<!!]]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(def DEFAULT_CONFIG {:db-name "sheet-bucket"
+                     :account-id d/PRO_ACCOUNT
+                     :endpoint "localhost:8998"
+                     :secret "secret"
+                     :access-key "key"
+                     :service "peer-server"
+                     :region "none"})
+
+(defn- read-schema! []
+  (->> (io/file "resources/schema.edn") slurp edn/read-string))
+
+(defn transact! [conn tx-data]
+  (<!! (d/transact conn {:tx-data tx-data})))
+
+(defn load-schema! [conn]
+  (transact! conn (read-schema!)))
+
+(defrecord Db [config]
+  c/Lifecycle
+  (start [this]
+    (assoc this :conn (<!! (client/connect config))))
+  (stop [this]
+    (dissoc this :conn)))
+
+(defn component [config]
+  (Db. (merge config DEFAULT_CONFIG)))
