758a633b531d298327723c2ecf0ad8198dabd91f
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-08 02:30:55 +0100

    Fix jetty issue: datomic uses older version than ring jetty adapter

--- project.clj
+++ project.clj
@@ -18,7 +18,9 @@
                  [ring/ring-jetty-adapter "1.6.3"]
                  [ring/ring-devel "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
-                 ;; [com.datomic/clj-client "0.8.606" :exclusions [org.eclipse.jetty/jetty-io]]
+                 [com.datomic/clj-client "0.8.606" :exclusions [org.eclipse.jetty/jetty-http
+                                                                org.eclipse.jetty/jetty-util
+                                                                org.eclipse.jetty/jetty-client]]
 
                  ;; CLJS
                  [reagent "0.7.0"]
