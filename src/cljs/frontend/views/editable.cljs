65f4ef50fb620f71eb7e64dd22d37a85fc74927a
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-10 21:05:00 +0100

    Auto focus and select editable nodes

--- src/cljs/frontend/views/editable.cljs
+++ src/cljs/frontend/views/editable.cljs
@@ -4,16 +4,23 @@
 
 (defn component []
   (let [editing (r/atom false)]
-    (fn [{:keys [on-change value]} children]
-      (if @editing
-        [:input {:type "text"
-                 :default-value value
-                 :on-key-down (fn [e]
-                                (when (#{ENTER ESC TAB} (.-which e))
-                                  (reset! editing false)
-                                  (on-change (.. e -target -value))))
-                 :on-blur (fn [e]
-                            (reset! editing false)
-                            (on-change (.. e -target -value)))}]
-        [:span {:on-click #(reset! editing true)}
-         children]))))
+    (r/create-class
+      {:component-did-update
+       (fn [this]
+         (when @editing
+           (.focus (r/dom-node this))
+           (.select (r/dom-node this))))
+       :reagent-render
+       (fn [{:keys [on-change value]} children]
+         (if @editing
+           [:input {:type "text"
+                    :default-value value
+                    :on-key-down (fn [e]
+                                   (when (#{ENTER ESC TAB} (.-which e))
+                                     (reset! editing false)
+                                     (on-change (.. e -target -value))))
+                    :on-blur (fn [e]
+                               (reset! editing false)
+                               (on-change (.. e -target -value)))}]
+           [:span {:on-click #(reset! editing true)}
+            children]))})))
