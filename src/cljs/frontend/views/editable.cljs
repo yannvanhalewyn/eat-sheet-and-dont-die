e8f85d326d92cc6190ab362a83ebcc201b964fc5
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-10 22:22:57 +0100

    Cleanup editable styling

--- src/cljs/frontend/views/editable.cljs
+++ src/cljs/frontend/views/editable.cljs
@@ -13,14 +13,15 @@
        :reagent-render
        (fn [{:keys [on-change value]} children]
          (if @editing
-           [:input {:type "text"
-                    :default-value value
-                    :on-key-down (fn [e]
-                                   (when (#{ENTER ESC TAB} (.-which e))
-                                     (reset! editing false)
-                                     (on-change (.. e -target -value))))
-                    :on-blur (fn [e]
-                               (reset! editing false)
-                               (on-change (.. e -target -value)))}]
+           [:input.editable
+            {:type "text"
+             :default-value value
+             :on-key-down (fn [e]
+                            (when (#{ENTER ESC TAB} (.-which e))
+                              (reset! editing false)
+                              (on-change (.. e -target -value))))
+             :on-blur (fn [e]
+                        (reset! editing false)
+                        (on-change (.. e -target -value)))}]
            [:span {:on-click #(reset! editing true)}
             children]))})))
