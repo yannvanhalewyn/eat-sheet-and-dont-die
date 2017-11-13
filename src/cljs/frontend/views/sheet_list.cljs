04755063d5fb893cccf8ddc53de7dccdc4d230d9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-13 19:58:32 +0100

    Style sheet list

--- src/cljs/frontend/views/sheet_list.cljs
+++ src/cljs/frontend/views/sheet_list.cljs
@@ -1,15 +1,20 @@
 (ns frontend.views.sheet-list
-  (:require [re-frame.core :refer [subscribe dispatch]]))
+  (:require [re-frame.core :refer [subscribe dispatch]]
+            [frontend.util.util :refer [stop-propagation]]))
 
 (defn component [{:keys [user]}]
   (let [sheets @(subscribe [:sub/sheets user])]
     [:div
-     [:button.btn {:on-click #(dispatch [:playlist/create-sheet (:db/id user)])} "Create!"]
+     [:button.btn.u-pull-right {:on-click #(dispatch [:playlist/create-sheet (:db/id user)])}
+      "+ New sheet"]
      [:h1 "My Sheets"]
-     (for [{:keys [:db/id :sheet/artist :sheet/title]} sheets]
-       ^{:key id}
-       [:div
-        [:a {:href (str "#sheets/" id)}
-         [:h2 title " - " [:small artist]]]
-        [:button.icon-btn.icon-btn--red {:on-click #(dispatch [:playlist/destroy-sheet id])}
-         [:i.material-icons "delete"]]])]))
+     [:div.u-margin-top
+      (for [{:keys [:db/id :sheet/artist :sheet/title]} sheets]
+        ^{:key id}
+        [:a.l-flex-row.l-space-between.sheet-list-item
+         {:href (str "#sheets/" id)}
+         [:div
+          [:div [:strong title]]
+          [:small artist]]
+         [:button.icon-btn.icon-btn--red {:on-click (stop-propagation #(dispatch [:playlist/destroy-sheet id]))}
+          [:i.material-icons "delete"]]])]]))
