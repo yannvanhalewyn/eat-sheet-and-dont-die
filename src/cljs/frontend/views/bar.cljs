eb02481c22377c3a7c55cf43e5f2a10971719c0d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-18 17:43:33 +0100

    Refactor attachments in bar component

--- src/cljs/frontend/views/bar.cljs
+++ src/cljs/frontend/views/bar.cljs
@@ -9,55 +9,52 @@
 (def height 30)
 (def width (* height svg-ratio))
 
-(defn component [{:keys [bar selected] :as props}]
-  (let [symbols (group-by :attachment/type (:bar/attachments bar))]
-    [:div.bar
-     ;; Segno's
-     ;; =======
-     (for [{:keys [db/id coord/x coord/y]} (:symbol/segno symbols)]
-       ^{:key id}
-       [draggable/component
-        {:class (str "music-symbol music-symbol--segno"
-                  (when (= id selected) " selected"))
+(defn attachment [{:keys [attachment bar selected]}]
+  (let [{:keys [db/id coord/x coord/y textbox/value]} attachment
+        props {:on-drag-end #(dispatch [:sheet/move-symbol (:db/id bar) id %])
+               :on-click #(dispatch [:sheet/select id])
+               :start-pos [x y]}
+        class (when selected "selected")]
+    (case (:attachment/type attachment)
+      :symbol/segno
+      [draggable/component
+       (assoc props
          :style {:width width :height height}
-         :on-drag-end #(dispatch [:sheet/move-symbol (:db/id bar) id %])
-         :on-click #(dispatch [:sheet/select id])
-         :start-pos [x y]}])
+         :class (str class " music-symbol music-symbol--segno"))]
 
-     ;; Chords
-     ;; ======
-     (let [width (/ 100 (count (:bar/chords bar)))]
-       (for [chord (sort-by :coll/position (:bar/chords bar))]
-         ^{:key (:db/id chord)}
-         [:div {:style {:display "inline-block"
-                        :width (str width "%")
-                        :margin-right "5px"}}
-          (if (= selected (:db/id chord))
-            [editable-chord {:chord chord}]
-            [displayed-chord {:chord (parse (:chord/value chord))
-                              :on-click #(dispatch [:sheet/select (:db/id chord)])}])]))
+      :symbol/coda
+      [draggable/component
+       (assoc props
+         :style {:width height :height height}
+         :class (str class " music-symbol music-symbol--coda")
+         :mode :align-right)]
 
-     ;; Texboxes
-     ;; ========
-     (for [{:keys [db/id coord/x coord/y textbox/value] :as texbox} (:attachment/textbox symbols)]
-       ^{:key id}
-       [draggable/component {:start-pos [x y]
-                             :class "draggable--textbox"
-                             :on-drag-end #(dispatch [:sheet/move-symbol (:db/id bar) id %])
-                             :on-click #(dispatch [:sheet/select id])}
-        [editable/component {:edit-trigger :on-double-click
-                             :on-change #(dispatch [:sheet/edit-textbox (:db/id bar) id %])
-                             :value value}
-         [:span value]]])
+      :attachment/textbox
+      [draggable/component
+       (assoc props :class (str class " draggable--textbox"))
+       [editable/component {:edit-trigger :on-double-click
+                            :on-change #(dispatch [:sheet/edit-textbox (:db/id bar) id %])
+                            :value value}
+        [:span value]]])))
 
-     ;; Coda's
-     ;; ======
-     (for [{:keys [db/id coord/x coord/y]} (:symbol/coda symbols)]
-       ^{:key id}
-       [draggable/component {:style {:width height :height height}
-                             :class (str "music-symbol music-symbol--coda"
-                                      (when (= id selected) " selected") )
-                             :mode :align-right
-                             :on-click #(dispatch [:sheet/select id])
-                             :on-drag-end #(dispatch [:sheet/move-symbol (:db/id bar) id %])
-                             :start-pos [x y]}])]))
+(defn component [{:keys [bar selected] :as props}]
+  [:div.bar
+
+   ;; Attachments
+   ;; =======
+   (for [{:keys [db/id] :as att} (:bar/attachments bar)]
+     ^{:key id}
+     [attachment {:attachment att :bar bar :selected (= id selected)}])
+
+   ;; Chords
+   ;; ======
+   (let [width (/ 100 (count (:bar/chords bar)))]
+     (for [chord (sort-by :coll/position (:bar/chords bar))]
+       ^{:key (:db/id chord)}
+       [:div {:style {:display "inline-block"
+                      :width (str width "%")
+                      :margin-right "5px"}}
+        (if (= selected (:db/id chord))
+          [editable-chord {:chord chord}]
+          [displayed-chord {:chord (parse (:chord/value chord))
+                            :on-click #(dispatch [:sheet/select (:db/id chord)])}])]))])
