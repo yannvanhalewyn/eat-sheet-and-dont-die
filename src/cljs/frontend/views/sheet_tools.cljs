4fb4f372291178065672af5bf28495cfcf905b73
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-18 19:36:43 +0100

    Instead of disabling tool buttons, hide them all together

--- src/cljs/frontend/views/sheet_tools.cljs
+++ src/cljs/frontend/views/sheet_tools.cljs
@@ -7,15 +7,14 @@
     (fn []
       (dispatch [:sheet/add-symbol type]))))
 
-(defn- tool [{:keys [type disabled]}]
-  [:button.sheet-tool-btn {:on-click (stop-propagation #(dispatch [:sheet/add-symbol type]))
-                           :disabled disabled}
+(defn- tool [{:keys [type]}]
+  [:button.sheet-tool-btn {:on-click (stop-propagation #(dispatch [:sheet/add-symbol type]))}
    [:div.sheet-tool {:class (str "sheet-tool--" (name type))}]])
 
 (defn component [{:keys [selection]}]
-  (let [disabled (not= :selection/chord (:selection/type selection))]
+  (when (= :selection/chord (:selection/type selection))
     [:div.sheet-tools
      (for [type [:bar/start-repeat :bar/end-repeat :attachment/segno :attachment/coda
                  :attachment/textbox]]
        ^{:key type}
-       [tool {:type type :disabled disabled}])]))
+       [tool {:type type}])]))
