204117cd8718f0cbad7085f27dfd189d82159228
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-30 02:25:23 +0100

    Setup /health uri for health checks

--- env/production/clj/umuses/env_middleware.clj
+++ env/production/clj/umuses/env_middleware.clj
@@ -7,7 +7,18 @@
                           :security {:ssl-redirect true
                                      :hsts true}}))
 
+(defn wrap-health-check
+  "Will respond with a status 200 on the path /health. It's important
+  to wrap this around SSL redirect, since EB's health check sends a
+  HTTP GET and expects a 200 response, not a 301 redirect."
+  [handler]
+  (fn [req]
+    (if (= (:uri req) "/health")
+      {:status 200}
+      (handler req))))
+
 (defn wrap-env-middleware [handler]
   (-> handler
     wrap-gzip
-    wrap-ssl-redirect))
+    wrap-ssl-redirect
+    wrap-health-check))
