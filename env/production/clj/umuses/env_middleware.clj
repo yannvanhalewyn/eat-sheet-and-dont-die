c87e522e0da2ad67c80610c61e555458a2344f26
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-30 01:54:49 +0100

    Enforce https in production

--- env/production/clj/umuses/env_middleware.clj
+++ env/production/clj/umuses/env_middleware.clj
@@ -1,5 +1,13 @@
 (ns umuses.env-middleware
-  (:require [ring.middleware.gzip :refer [wrap-gzip]]))
+  (:require [ring.middleware.gzip :refer [wrap-gzip]]
+            [ring.middleware.defaults :refer [wrap-defaults]]))
+
+(defn wrap-ssl-redirect [handler]
+  (wrap-defaults handler {:proxy true
+                          :security {:ssl-redirect true
+                                     :hsts true}}))
 
 (defn wrap-env-middleware [handler]
-  (wrap-gzip handler))
+  (-> handler
+    wrap-gzip
+    wrap-ssl-redirect))
