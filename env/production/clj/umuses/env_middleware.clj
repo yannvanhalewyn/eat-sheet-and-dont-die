19412633b15f2b131936dc02b43f129f346479ba
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2017-11-30 03:02:20 +0100

    Wrap health check based on user agent and not uri
    since EB keeps pingin "/" io "/health"

--- env/production/clj/umuses/env_middleware.clj
+++ env/production/clj/umuses/env_middleware.clj
@@ -2,23 +2,24 @@
   (:require [ring.middleware.gzip :refer [wrap-gzip]]
             [ring.middleware.defaults :refer [wrap-defaults]]))
 
-(defn wrap-ssl-redirect [handler]
-  (wrap-defaults handler {:proxy true
-                          :security {:ssl-redirect true
-                                     :hsts true}}))
-
-(defn wrap-health-check
-  "Will respond with a status 200 on the path /health. It's important
+(defn- wrap-health-check
+  "Will respond with a status 200 for user agents starting with
+  \"ELB-Health\" (like \"ELB-HealthChecker/2.0\"). It's important
   to wrap this around SSL redirect, since EB's health check sends a
   HTTP GET and expects a 200 response, not a 301 redirect."
   [handler]
   (fn [req]
-    (if (= (:uri req) "/health")
+    (if (.startsWith (get-in req [:headers "user-agent"]) "ELB-Health")
       {:status 200}
       (handler req))))
+
+(defn- wrap-ssl-redirect [handler]
+  (-> handler
+    (wrap-defaults {:proxy true
+                    :security {:ssl-redirect true :hsts true}})
+    wrap-health-check))
 
 (defn wrap-env-middleware [handler]
   (-> handler
     wrap-gzip
-    wrap-ssl-redirect
-    wrap-health-check))
+    wrap-ssl-redirect))
